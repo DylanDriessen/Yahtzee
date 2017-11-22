0b7dbfdc7fbd3b6605a1e7b776fc190af7a329f9
remidelk <remidelk@hotmail.com>
2017-11-22 10:59:25 +0100

    domainexception

---
+++ OOO_Part2/yahtzee/src/model/DomainException.java
@@ -0,0 +1,25 @@
+package model;
+
+public class DomainException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+
+	public DomainException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	public DomainException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
