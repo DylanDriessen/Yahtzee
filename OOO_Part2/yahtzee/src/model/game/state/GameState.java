cc732e65b08769e2146df2c945766c5ec04f129e
DylanDriessen <dylandriessen1@gmail.com>
2017-11-28 16:15:07 +0100

    lol

--- OOO_Part2/yahtzee/src/model/game/state/GameState.java
+++ OOO_Part2/yahtzee/src/model/game/state/GameState.java
@@ -2,9 +2,12 @@ package model.game.state;
 
 import model.board.Dice;
 import model.game.Game;
+import model.player.Player;
 
 public interface GameState {
 
 	void leftClicked(Dice dices);
+
+	void leftClicked(Dice dices, Player player);
 	
 }
--- OOO_Part2/yahtzee/src/model/game/state/StartedState.java
+++ OOO_Part2/yahtzee/src/model/game/state/StartedState.java
@@ -1,11 +1,23 @@
 package model.game.state;
 
 import model.board.Dice;
+import model.game.Game;
+import model.player.Player;
 
 public class StartedState implements GameState {
+	
+	Game game = new Game();
+	
+	public StartedState(Game game){
+		this.game = game;
+	}
 
 	@Override
-	public void leftClicked(Dice dices) {
+	public void leftClicked(Dice dices, Player player) {
+		if(dices.rolled()){
+			return;
+		}
+		if()
 		// TODO Auto-generated method stub
 		
 	}
