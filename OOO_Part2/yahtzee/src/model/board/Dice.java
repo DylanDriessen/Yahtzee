162c62636744e7f9ec95be710b8dbcb65ace8995
remidelk <remidelk@hotmail.com>
2017-12-06 16:08:25 +0100

    veel gedeleted --

--- OOO_Part2/yahtzee/src/model/board/Dice.java
+++ OOO_Part2/yahtzee/src/model/board/Dice.java
@@ -1,94 +1,30 @@
 package model.board;
 
-import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-
-import javafx.animation.RotateTransition;
-import javafx.beans.binding.IntegerExpression;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.geometry.Pos;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
-import javafx.util.Duration;
-import model.factory.DiceFactory;
 
 public class Dice extends StackPane {
 	
-	/*public static final int MIN_VALUE = 1;
-	public static final int MAX_VALUE = 6;
-	private int rand = 0;
-	ArrayList<String> numbers = new ArrayList<>();
-	private boolean hold = false;*/
 	private boolean rolled = false;
-	
-	//Text text = new Text();
 	private int eyes;
 	
 	public Dice() {
 		
-		/*Rectangle rect = new Rectangle(50 , 50);
-		text.setFill(Color.WHITE); // de cijfers in de dobbelstenen
-		text.setText("0");
-		this.setAlignment(Pos.CENTER);
-		getChildren().addAll(rect,text);
-		*/
 	}
 	
 	public int getEyes() {
 		return eyes;
 	}
-	
-	
-
-
 
 	public void roll(){
 		eyes = this.randomize();
 		
-		/*RotateTransition r = new RotateTransition(Duration.seconds(1), this); // this als een node want we extenden van StackPane
-		r.setFromAngle(0);
-		r.setToAngle(360); // de graden dat de dobbelsteen gaat draaien
-		rand = this.randomize();
-		text.setText(Integer.toString(rand));
-		numbers.add(Integer.toString(rand));
-		System.out.println(numbers);*/
-		
 	}
-	
-
-	
-	
-	
-	
-	/*public void replaceDice(Dice dice, int y){
-
-		dice.setHold(true);
-
-		dice.setTranslateX(100 + y);
-		dice.setTranslateY(100 );
-		
-		
-	}
-	*/
 	private int randomize(){
 		Random random = new Random();
 		int numberShowing = random.nextInt(6)+1;
 		return numberShowing;
 	}
-	
-	
-	/*private void setHold(boolean hold) {
-		this.hold = hold;
-	}
-	
-	public boolean getHold() {
-		return this.hold;
-	}
-	*/
-	
 	public void setRolled(boolean roll) {
 		this.rolled = roll;
 	}
--- OOO_Part2/yahtzee/src/model/game/Game.java
+++ OOO_Part2/yahtzee/src/model/game/Game.java
@@ -1,11 +1,6 @@
 package model.game;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import exception.DomainException;
 import model.player.Player;
 
 public class Game {
@@ -25,8 +20,6 @@ public class Game {
 		System.out.println(persons);
 		return persons;
 	}
-	
-	
 	public void addPersonalGame(String naam){
 		Player player = new Player(naam);
 		PersonalGame pg = new PersonalGame(player);
@@ -38,35 +31,6 @@ public class Game {
  		game.remove(personalGame);
  		
  	}
- 	//public Player playerIsInGame(String naam){
- 	//	if(naam == null){
-	//		throw new DomainException("No name given");
-	//	}
- 	//	
-	//	for(Player s: game.){
-	//		if(s.getTrun()){
-	//			return s;
-	//		}
-	//	}
-	//	
-	//	throw new DomainException("Player does not exists");
-	//	
-	//}
- 	//
- 	//public PersonalGame getNextPlayer(String naam){
- 	//	return getPlayer(naam);
- 	//	int i = 0;
- 	//	for(PersonalGame s: game){
-	//		if(s.getNaam().equals(naam)){
-	//			return spelers.get(i+1).getNaam();
-	//		}if(i == spelers.size()){
-	//			return spelers.get(0).getNaam();
-	//		}else{
-	//			i++;
-	//		}
-	//	}
-	//	return spelers.get(i+1).getNaam();
- 	//}
  	
  	public int getIndexNextPersonalGame(){
  		return indexNextPersonalGame;
@@ -96,22 +60,6 @@ public class Game {
  		}
  		else this.setIndexNextPersonalGame(-1);
  	}
- 	 
- 	
- 	/*public PersonalGame inGamePlayer(String name) {
- 		if(game.isEmpty()) {
- 			throw new DomainException("No players");
- 		}
- 		
- 		for (PersonalGame player : game) {
- 			if (player.getPlayer().getNaam().equals(name)) {
- 				return player;
- 			}
- 		}
- 		
- 		throw new IllegalArgumentException("player does not exist");
- 	}*/
- 	
  	
  	public ArrayList<String> getAllPlayersNames(){
  		ArrayList<String> games = new ArrayList<>();
@@ -120,36 +68,5 @@ public class Game {
  		}
 		return games;	
  		
- 	}
- 	
- 	//public void setNextPlayer() {
- 	//	int i = 0;
- 	//	int x = 0;
- 	//	boolean found = false;
- 	//	
- 	//	if(spelers == null || spelers.isEmpty() || spelers.size() <= 1) {
- 	//		throw new DomainException("Give at least 2 players");
- 	//	}
- 	//	
- 	//	for (Player player : spelers) {
- 	//		if(player.getTrun() && !spelers.get(spelers.size()-1).equals(player)) {
- 	//			found = true;
- 	//			player.setTurn(false);
- 	//			x = i + 1;
- 	//		} else if (spelers.get(spelers.size()-1).equals(player)) {
- 	//			x = 0;
- 	//		}
- 	//		i++;
- 	//	}
- 	//	
- 	//	if (!found) {
- 	//		spelers.get(0).setTurn(true);
- 	//	} else {
- 	//		spelers.get(x).setTurn(true);
- 	//	}
- 	//}
- 	
- 	
+ 	}	
  }
-
-
