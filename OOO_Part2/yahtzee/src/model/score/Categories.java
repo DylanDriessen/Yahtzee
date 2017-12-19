f85384e7cddc7e4d51a4a0213e03faa8f9c3fe2d
DylanDriessen <dylandriessen1@gmail.com>
2017-12-19 23:04:25 +0100

    score test

--- OOO_Part2/yahtzee/src/model/score/Categories.java
+++ OOO_Part2/yahtzee/src/model/score/Categories.java
@@ -2,19 +2,19 @@ package model.score;
 
 public enum Categories {
 	
-	ACES("Aces", "0"),
-	TWOS("Twos", "0"),
-	THREES("Threes", "0"),
-	FOURS("Fours", "0"),
-	FIVES("Fives", "0"),
-	SIXES("Sixes", "0"),
-	THREE_OF_A_KIND("Three of a kind", "0"),
-	FOUR_OF_A_KIND("Four of a kind", "0"),
-	FULL_HOUSE("Full house", "0"),
-	SMALL_STRAIGHT("Small straight", "0"),
-	LARGE_STRAIGHT("Large straight", "0"),
-	YAHTZEE("Yahtzee", "0"),
-	CHANCE("Chance", "0");
+	ACES("Aces", null),
+	TWOS("Twos", null),
+	THREES("Threes", null),
+	FOURS("Fours", null),
+	FIVES("Fives", null),
+	SIXES("Sixes", null),
+	THREE_OF_A_KIND("Three of a kind", null),
+	FOUR_OF_A_KIND("Four of a kind", null),
+	FULL_HOUSE("Full house", null),
+	SMALL_STRAIGHT("Small straight", null),
+	LARGE_STRAIGHT("Large straight", null),
+	YAHTZEE("Yahtzee", null),
+	CHANCE("Chance", null);
 	
 	private String categorie;
 	private String score;
