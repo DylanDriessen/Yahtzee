9c06a086335f0fec286f6305985da06b234ae6f0
DylanDriessen <dylandriessen1@gmail.com>
2017-12-19 23:01:41 +0100

    Score test

--- OOO_Part2/yahtzee/src/model/score/Categories.java
+++ OOO_Part2/yahtzee/src/model/score/Categories.java
@@ -2,28 +2,34 @@ package model.score;
 
 public enum Categories {
 	
-	ACES("Aces"),
-	TWOS("Twos"),
-	THREES("Threes"),
-	FOURS("Fours"),
-	FIVES("Fives"),
-	SIXES("Sixes"),
-	THREE_OF_A_KIND("Three of a kind"),
-	FOUR_OF_A_KIND("Four of a kind"),
-	FULL_HOUSE("Full house"),
-	SMALL_STRAIGHT("Small straight"),
-	LARGE_STRAIGHT("Large straight"),
-	YAHTZEE("Yahtzee"),
-	CHANCE("Chance");
+	ACES("Aces", "0"),
+	TWOS("Twos", "0"),
+	THREES("Threes", "0"),
+	FOURS("Fours", "0"),
+	FIVES("Fives", "0"),
+	SIXES("Sixes", "0"),
+	THREE_OF_A_KIND("Three of a kind", "0"),
+	FOUR_OF_A_KIND("Four of a kind", "0"),
+	FULL_HOUSE("Full house", "0"),
+	SMALL_STRAIGHT("Small straight", "0"),
+	LARGE_STRAIGHT("Large straight", "0"),
+	YAHTZEE("Yahtzee", "0"),
+	CHANCE("Chance", "0");
 	
 	private String categorie;
+	private String score;
 	
-	Categories(String categorie){
+	Categories(String categorie, String score){
 		this.categorie = categorie;
+		this.score = score;
 	}
 	
 	public String getCategorie() {
 	    return categorie;
+	}
+	
+	public String getScore(){
+		return score;
 	}
 
 }
--- OOO_Part2/yahtzee/src/view/scoreboard/Scoreboard.java
+++ OOO_Part2/yahtzee/src/view/scoreboard/Scoreboard.java
@@ -1,13 +1,10 @@
 package view.scoreboard;
 
-import javafx.beans.property.SimpleIntegerProperty;
+
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import model.score.Categories;
-import model.score.Category;
-import model.score.SimpleCatagoryStrategy;
 
 public class Scoreboard {
 
@@ -19,16 +16,16 @@ public class Scoreboard {
 		TableView<Categories> table = new TableView<Categories>();
 
 		TableColumn<Categories, String> catCol = new TableColumn<>("Categorie");
-		catCol.setMinWidth(100);
+		catCol.setMinWidth(200);
 		catCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCategorie()));
 
 		//Score kolom
-		TableColumn<SimpleCatagoryStrategy, Integer> scoreCol = new TableColumn<>("Score");
+		TableColumn<Categories, String> scoreCol = new TableColumn<>("Score");
 		scoreCol.setMinWidth(100);
-//		scoreCol.setCellValueFactory(cellData -> score);
+		scoreCol.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getScore()));
 
 		table.getItems().addAll(Categories.values());
-		table.getColumns().add(catCol);
+		table.getColumns().addAll(catCol, scoreCol);
 		
 		return table;
 	}
